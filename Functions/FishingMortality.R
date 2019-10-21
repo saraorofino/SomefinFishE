8dbf5a190ccbe8dde0135214795d276639d313ff
Sara Orofino <saraorofino@bren.ucsb.edu>
2019-10-21 12:45:39 -0700

    Updated functions to include a second possible function based on different parameterization (from Costello et al 2016/ Gaines et al 2018)

--- Functions/FishingMortality.R
+++ Functions/FishingMortality.R
@@ -3,10 +3,17 @@
 
 
 #Open Access fishing mortality is the rate would yield 0.3*BMSY
-
-F_oa <- function(r,p){
+F_oa1 <- function(r,p){
   (r/p) * (1-
              ((0.3 ^ p)/
                 ((p+1) ^ (1+p)))
   )
 }
+
+
+####Parameterized from Gaines et al 2018 
+F_oa2 <- function(g, phi){
+  ((phi + 1) / phi) * g * (
+    (0.3^phi) / (1 + phi)
+  )
+} 
